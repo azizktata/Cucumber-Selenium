a8654df021e1e5b2283b612e3b0fdb5cc3cb3f0e
azizktata <65952489+azizktata@users.noreply.github.com>
2023-11-21 10:37:20 +0100

    first commit

---
+++ src/test/java/StepDefinition.java
@@ -0,0 +1,44 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class StepDefinition {
+    public WebDriver driver;
+    FirefoxBinary firefoxBinary;
+
+    @Before
+    public void createWebDriver(){
+        firefoxBinary = new FirefoxBinary();
+    }
+    @After
+    public void quitWebDriver(){
+        driver.quit();
+    }
+    @Given("I open google search page")
+    public void i_open_google_search_page() {
+        firefoxBinary.addCommandLineOptions("--headless");
+        firefoxBinary.addCommandLineOptions("--no-sandbox");
+        System.setProperty("webdriver.gecko.driver","/usr/bin/geckodriver");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        driver = new FirefoxDriver(firefoxOptions);
+        driver.get("https://www.google.fr");
+    }
+    @When("I lookup the word {string}")
+    public void i_lookup_the_word(String string) {
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys(string);
+        driver.findElement(By.name("btnk")).click();
+
+    }
+    @Then("Search results display the word {string}")
+    public void search_results_display_the_word(String string) {
+        System.out.println(driver.getTitle().contains(string));
+    }
+}
---
+++ src/test/resources/googlesearch.feature
@@ -0,0 +1,7 @@
+Feature: Google search
+  as user i want test google search function
+
+  Scenario: basic search
+    Given I open google search page
+    When I lookup the word "Selenium"
+    Then Search results display the word "Selenium"
